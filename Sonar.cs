4a17ca7bc23f1fee854aab76f8d13282e48bb5ee
NovaSkyrim <100930779+NovaSkyrim@users.noreply.github.com>
2024-11-03 23:06:54 +0100

    Update : Sonar.cs of Maëva

--- Sonar.cs
+++ Sonar.cs
@@ -4,24 +4,53 @@ using UnityEngine;
 
 public class Sonar : MonoBehaviour
 {
-    public Material sonarMat;
+    // matériau pour pouvoir envoyer au sous marin, et du coup au shader, la position du sous marin
+    [SerializeField] Material sonarMat; // champ sérialisé, donc visible dans l'inspecteur mais privé
     SubmarineNavigator navigator;
 
     // Start is called before the first frame update
     void Start()
     {
         navigator = FindObjectOfType<SubmarineNavigator>();
+
+        StartCoroutine(UpdatePositions());
     }
+
+    // récupérer fréquence de scan, la vitesse (dans le shader)
+    // quand time*speed arrive au dela de scan freq => ça revient à 0 (faire time -= threshold) (pas = 0 sinon on perd des millisecondes, et donc pas synchro) 
+    void Update()
+    {
+        if ()
+        {
+            StartCoroutine(UpdatePositions());
+        }
+    }
+
+    //IEnumerator UpdatePositions()
+    //{
+    //    float scanFrequency = 1;
+    //    while (true)
+    //    {
+    //        yield return new WaitForSeconds(scanFrequency);
+    //        // update les positions
+    //        // navigator.submarinePosition ...
+    //        // passer la nextPos en pos, et mettre une nouvelle nextPos
+
+    //        //sonarMat.SetVector("_Position", submarineLastPosition); récupérer les positions de submarine navigator
+    //        //sonarMat.SetVector("_NextPosition", submarinePosition);
+    //    }
+    //}
 
     IEnumerator UpdatePositions()
     {
-        float scanFrequency = 1;
-        while (true)
-        {
-            yield return new WaitForSeconds(1);
-            // update les positions
-            // navigator.submarinePosition ...
-            // passer la nextPos en pos, et mettre une nouvelle nextPos
+        
+        yield return null;
+        // update les positions
+        // navigator.submarinePosition ...
+        // passer la nextPos en pos, et mettre une nouvelle nextPos
+
+        //sonarMat.SetVector("_Position", submarineLastPosition); récupérer les positions de submarine navigator
+        //sonarMat.SetVector("_NextPosition", submarinePosition);
         }
     }
 }
